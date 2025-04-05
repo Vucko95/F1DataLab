648a0ef7cefeee9da722b007d3e2a4d47b2e4507
Vucko95 <tyger995@gmail.com>
2025-04-05 21:20:51 +0100

    Add Resposivnes for Costructor table Comp

--- f1datalab/components/constructors/ConstructorsTable.tsx
+++ f1datalab/components/constructors/ConstructorsTable.tsx
@@ -17,52 +17,53 @@ interface ConstructorStanding {
 
 export function ConstructorsTable({ year }: TableCardProps) {
   const [constructors, setConstructors] = useState<ConstructorStanding[]>([]);
-  const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data: ConstructorStanding[] = await fetchConstructorStandingsYear(year);
-        setConstructors(data);
-      } catch (error) {
-        console.error("Error fetching constructor standings:", error);
-      }
-    };
-    if (isMounted) {
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data: ConstructorStanding[] = await fetchConstructorStandingsYear(year);
+          setConstructors(data);
+        } catch (error) {
+          console.error("Error fetching driver standings:", error);
+        }
+      };
       fetchData();
-    }
-  }, [year, isMounted]);
-
-  if (!isMounted) {
-    return null;
-  }
+    }, [year]);
 
   return (
-<Card className="p-1 h-full flex flex-col">
-<CardHeader className="p-2 pb-4 flex justify-center items-center text-lg">Constructor Standigns </CardHeader>
-        <Table >
-        <TableHeader className="sticky top-0 bg-background z-10">
-        <TableRow>
-              <TableHead className="text-center">Constructor</TableHead>
-              <TableHead className="text-center">Total Points</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {constructors.map((constructor) => (
-              <TableRow key={constructor.constructorId} className="text-center">
-                <TableCell className="flex justify-center gap-x-2" >
-                  <Image src={`/images/constructors/${constructor.constructorId}.webp`} alt="Constructor Logo" width={25} height={25} className="rounded-sm" />
-                  {constructor.constructor_name}
-                </TableCell>
-                <TableCell>{constructor.total_points}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-    </Card>
+<Card className="p-4 h-full flex flex-col">
+  <CardHeader className="p-4 pb-6 flex justify-center items-center responsive-header">
+    Constructor Standings
+  </CardHeader>
+
+  <Table className="responsive-text">
+    <TableHeader className="sticky top-0 bg-background z-10">
+      <TableRow>
+        <TableHead className="text-center responsive-text">Team</TableHead>
+        <TableHead className="text-center responsive-text">Total Points</TableHead>
+      </TableRow>
+    </TableHeader>
+
+    <TableBody>
+      {constructors.map((constructor) => (
+        <TableRow key={constructor.constructorId} className="text-center responsive-text">
+          <TableCell className="flex justify-center items-center gap-x-3">
+            <Image
+              src={`/images/constructors/${constructor.constructorId}.webp`}
+              alt="Constructor Logo"
+              width={35}
+              height={35}
+              className="rounded-sm responsive-logo"
+            />
+            {constructor.constructor_name}
+          </TableCell>
+          <TableCell>{constructor.total_points}</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</Card>
+
+
   );
 }
