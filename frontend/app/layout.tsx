2408148e9b578f467164fe9bee32794f0c0e4447
Vucko95 <tyger995@gmail.com>
2025-06-07 21:01:06 +0100

    Refactor RootLayout to use SidebarInset and improve layout structure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import { ThemeProvider } from "../components/ui/theme-provider";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/structure/app-sidebar";
+import { ModeToggle } from "@/components/ui/ModeToggle";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -12,11 +14,23 @@ export default function RootLayout({
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <SidebarProvider>
-            <AppSidebar />
-            <main>
-              {/* <SidebarTrigger /> TRIGGER WHICH SHOWS / HIDES SIDEBAR  */}
-              {children}
-            </main>
+            <div
+              className="group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar"
+              style={
+                {
+                  "--sidebar-width": "13rem",
+                  "--sidebar-width-icon": "3rem"
+                } as React.CSSProperties & Record<string, string>
+              }
+            >
+              <AppSidebar />
+              <SidebarInset className="relative p-4 overflow-y-auto">
+                <div className="fixed top-4 right-4 z-50">
+                  <ModeToggle />
+                </div>
+                {children}
+              </SidebarInset>
+            </div>
           </SidebarProvider>
         </ThemeProvider>
       </body>
