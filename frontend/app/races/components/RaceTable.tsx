f3b5026e09d54d4c5bed3727237078851e206ba4
Vucko95 <tyger995@gmail.com>
2025-06-14 19:51:55 +0100

    Refactor RaceTable component for improved layout and error handling; add position change display for drivers

--- frontend/app/races/components/RaceTable.tsx
+++ frontend/app/races/components/RaceTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent } from "@/components/ui/card";
 import { fetchRaceDetails } from "@/app/services/api";
 import { Button } from "@/components/ui/button";
@@ -19,9 +19,9 @@ interface RaceResults {
   surname: string;
   nationality: string;
   position: number;
-  time: string
+  grid: number;
+  time: string | null;
 }
-
 
 export function RaceTable({ raceId }: RaceIdProps) {
   const [drivers, setRaceResults] = useState<RaceResults[]>([]);
@@ -34,7 +34,7 @@ export function RaceTable({ raceId }: RaceIdProps) {
         const data: RaceResults[] = await fetchRaceDetails(raceId);
         setRaceResults(data);
       } catch (error) {
-        console.error("Error fetching driver standings:", error);
+        console.error("Error fetching race results:", error);
       }
     };
     fetchData();
@@ -47,14 +47,15 @@ export function RaceTable({ raceId }: RaceIdProps) {
   );
 
   return (
-    <Card >
-      <CardContent>
+    <Card className="h-full">
+      <CardContent className="h-full flex flex-col justify-between">
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead className="text-center">Position</TableHead>
+              <TableHead className="text-center">Pos</TableHead>
               <TableHead className="text-center">N</TableHead>
               <TableHead className="text-center">Driver</TableHead>
+              <TableHead className="text-center">Change</TableHead>
               <TableHead className="text-center">Constructor</TableHead>
               <TableHead className="text-center">Time</TableHead>
             </TableRow>
@@ -62,40 +63,53 @@ export function RaceTable({ raceId }: RaceIdProps) {
           <TableBody>
             {paginatedDrivers.map((driver) => {
               const nationalityFlag = nationalityToFlag[driver.nationality] || "us";
+              const positionChange = driver.grid - driver.position;
+              let positionChangeText: string;
+              let positionChangeColorClass: string;
+
+              if (positionChange > 0) {
+                positionChangeText = `+${positionChange}`;
+                positionChangeColorClass = 'text-green-500';
+              } else if (positionChange < 0) {
+                positionChangeText = `${positionChange}`;
+                positionChangeColorClass = 'text-red-500';
+              } else {
+                positionChangeText = 'N';
+                positionChangeColorClass = 'text-gray-500';
+              }
+
               return (
                 <TableRow key={driver.driverId} className="text-center">
                   <TableCell>{driver.position}</TableCell>
-                  <TableCell >
+                  <TableCell>
                     <Image src={`https://flagcdn.com/${nationalityFlag}.svg`} alt={`${driver.nationality} Flag`} width={20} height={20} className="rounded-sm" />
                   </TableCell>
                   <TableCell>{driver.forename} {driver.surname}</TableCell>
+                  <TableCell className={positionChangeColorClass}>
+                    {positionChangeText}
+                  </TableCell>
                   <TableCell className="flex justify-center">
                     <Image src={`/images/constructors/${driver.constructorId}.webp`} alt="Constructor Logo" width={25} height={25} className="rounded-sm" />
                   </TableCell>
-                  <TableCell>{driver.time}</TableCell>
+                  <TableCell>{driver.time || 'N/A'}</TableCell>
                 </TableRow>
               );
             })}
           </TableBody>
         </Table>
-
         <div className="flex justify-between w-full pt-4 ">
           <Button
             className="bg-sidebar text-foreground border-gray-600 hover:bg-gray-700"
             onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
             disabled={currentPage === 1}>Prev
           </Button>
-
           <Button
             className="bg-sidebar text-foreground border-gray-600 hover:bg-gray-700"
-
             onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
             disabled={currentPage === totalPages}>Next
           </Button>
         </div>
-
-    </CardContent>
-  </Card>
+      </CardContent>
+    </Card>
   );
 }
-
