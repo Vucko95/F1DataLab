9e569f3339fee7d7ef03cacfe81fea2e11d0e240
Vucko95 <tyger995@gmail.com>
2026-01-02 22:56:42 +0000

    Remove Nginx mappings/Add gitignore

--- certbot/renewal-hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Post-renewal hook to reload nginx without downtime
-
-echo "Certificate renewed successfully at $(date)"
-docker exec f1_nginx nginx -s reload
-echo "Nginx configuration reloaded"
